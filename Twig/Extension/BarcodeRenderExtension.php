56074df5cff0cf6d3abaf762b4e59e2ca35c0f6c
dbla <dbla>
2017-12-15 17:18:01 -0500

    Updated twig

--- Twig/Extension/BarcodeRenderExtension.php
+++ Twig/Extension/BarcodeRenderExtension.php
@@ -44,8 +44,8 @@ class BarcodeRenderExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'mopa_barcode_url' => new \Twig_SimpleFilter($this, 'url'),
-            'mopa_barcode_path' => new \Twig_SimpleFilter($this, 'path'),
+            'mopa_barcode_url' => new \Twig_SimpleFunction($this, 'url'),
+            'mopa_barcode_path' => new \Twig_SimpleFunction($this, 'path'),
         );
     }
 
